b94bb1dc86825cc34cdf3ae2c23e9dda9a0afd1c
Antonio Celso <celso@Gauna>
2017-04-25 18:39:46 -0300

    iniciando respositorio

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sistemasApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sistemasApp
+ */
+angular.module('sistemasApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sistemasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sistemasApp
+ */
+angular.module('sistemasApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
